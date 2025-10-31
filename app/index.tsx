77e327a55a979fe3c909517bdca5b68f1c60874f
Jack Johnson <jrj221@byu.edu>
2025-10-30 20:28:58 -0600

    made header, practicing making arcing text with svg paths

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'expo-router'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
 const Home = () => {
   return (
     <View style={styles.container}>
+        <View style={styles.header}></View>
+        <Image source={require("../assets/images/cookingPlaceholder.png")} style={styles.img}></Image>
         <Text style={[styles.title, {color: 'darkblue'}]}>Let... Me... COOK</Text>
 
         <Link style={styles.link} href="/pantry">Pantry</Link>
@@ -18,10 +20,20 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
+    header: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        backgroundColor: "#296968",
+        height: 70,
+        width: "100%",
+        boxShadow: "0px 0px 10px black"
+    },
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: "#E1AA57"
     },
     title: {
         fontWeight: 'bold',
