c686cee9d4401be71a35ac3bef54fd6989b3df9a
Jack Johnson <jrj221@byu.edu>
2025-11-20 20:28:53 -0700

    made an add button that modifies the ingredient list and rerendered the pantry

--- app/pantry.tsx
+++ app/pantry.tsx
@@ -1,11 +1,22 @@
 import { GLOBAL_STYLES } from '@/theme';
 import Checkbox from 'expo-checkbox';
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 
 export default function pantry() {
   const [checkedItems, setCheckedItems] = useState<{[key: string]: boolean}>({});
+  const [showAddBox, setShowAddBox] = useState(false);
+  const [ingredients, setIngredients] = useState([
+    "Milk",
+    "Butter",
+    "Cheddar Cheese",
+    "Greek Yogurt",
+    "Spinach",
+    "Cereal",
+    "Oats"
+  ]);
+
 
   const toggleCheckbox = (ingredient: string) => {
     setCheckedItems(prev => ({
@@ -14,35 +25,14 @@ export default function pantry() {
     }));
   };
 
-  const ingredients = [
-    "Milk",
-    "Butter",
-    "Cheddar Cheese",
-    "Greek Yogurt",
-    "Spinach",
-    "Tomatoes",
-    "Onions",
-    "Garlic",
-    "Bell Peppers",
-    "Carrots",
-    "Celery",
-    "Potatoes",
-    "Chicken Breast",
-    "Ground Beef",
-    "Tofu",
-    "Canned Beans",
-    "Pasta",
-    "Rice",
-    "Quinoa",
-    "Olive Oil",
-    "Soy Sauce",
-    "Peanut Butter",
-    "Jam",
-    "Bread",
-    "Tortillas",
-    "Cereal",
-    "Oats"
-  ];
+  const addIngredient = (ingredient: string) => {
+    if (ingredient.trim() != "" && !ingredients.includes(ingredient)) {
+      setIngredients(prev => [...prev, ingredient]);
+    }
+    setShowAddBox(false);
+  }
+
+  const toggleAddIngredientButton = () => setShowAddBox(!showAddBox);
   
 
   return (
@@ -55,6 +45,18 @@ export default function pantry() {
             </View>
           ))}
         </ScrollView>
+        {showAddBox && 
+          <View style={styles.addIngredientBox}>
+            <TextInput placeholder='What would you like to add to your pantry?'
+                      placeholderTextColor={"white"}
+                      onSubmitEditing={(e) => addIngredient(e.nativeEvent.text)}
+                      style={styles.addIngredientText}
+            ></TextInput>
+          </View>
+        }
+        <TouchableOpacity onPress={toggleAddIngredientButton} style={styles.addIngredientButton} activeOpacity={1}>
+          <Text style={styles.addIcon}>+</Text>
+        </TouchableOpacity>
     </View>
   )
 }
@@ -81,5 +83,48 @@ const styles = StyleSheet.create({
   },
   checkbox: {
     marginLeft: 15,
-  }
+    borderColor: "black",
+  },
+  addIngredientButton: {
+    position: "absolute",
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+    backgroundColor: "green",
+    bottom: 20,
+    right: 20,
+    shadowOffset: { width: 4, height: 4 },
+    shadowColor: "darkgreen",
+    shadowOpacity: 1,
+    shadowRadius: 0,
+
+    // control the + icon
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  addIcon: {
+    color: "white",
+    fontSize: 44,
+  },
+  addIngredientBox: {
+    width: 330,
+    height: 50,
+    borderRadius: 20,
+    backgroundColor: "#B86F1C",
+    position: "absolute",
+    top: "40%",
+    display: "flex",
+    justifyContent: "center",
+
+    shadowOffset: { width: 4, height: 4 },
+    shadowColor: "#9E5F18",
+    shadowOpacity: 1,
+    shadowRadius: 0,
+  },
+  addIngredientText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16,
+  },
 })
